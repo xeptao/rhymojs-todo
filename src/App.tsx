1851f8ff9699b92f25ec99f8e76c54570eeab9c4
xeptao <xeptaowastaken@gmail.com>
2022-05-02 15:14:24 +0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "./App.module.css";
 
 export const App: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <header className={styles.AppHeader}>
+        <div>
+          <h1>To Do App with Rhymo.</h1>
+          <p>
+            This is an example of the implementation of Rhymo. <br /> See the{" "}
+            <a href="https://github.com/xeptao/rhymojs-todo">
+              GitHub repository
+            </a>{" "}
+            for the code.
+          </p>
+        </div>
+      </header>
+    </>
+  );
 };
